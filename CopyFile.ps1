f608e31b1eb948b31b0b87065b129e978d82825e
Shais766 <143799666+Eagle766@users.noreply.github.com>
2025-06-15 16:41:31 +0530

    Update CopyFile.ps1
    Code Insert

--- CopyFile.ps1
+++ CopyFile.ps1
@@ -1,1 +1,24 @@
+function Copy-File {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$SourcePath,
+
+        [Parameter(Mandatory=$true)]
+        [string]$DestinationPath
+    )
+
+    try {
+        if (Test-Path $SourcePath) {
+           Copy-Item -Path "$SourcePath\*" -Destination $DestinationPath -Recurse -Force
+            Write-Host "File copied successfully from '$SourcePath' to '$DestinationPath'."
+        } else {
+            Write-Warning "Source file does not exist: $SourcePath"
+        }
+    }
+    catch {
+        Write-Error "Error copying file: $_"
+    }
+}
+
+Copy-File -SourcePath "F:\File1" -DestinationPath "F:\File2"
 
